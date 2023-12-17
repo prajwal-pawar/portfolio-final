80197a64238a6eb26c021078bf689558f46dd9a3
Prajwal <prajwalpawar2303@gmail.com>
2023-12-18 00:35:21 +0530

    added about me para in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,9 +12,14 @@ const About = () => {
     >
       <h2 className="about-title">About Me</h2>
       <p className="about-info">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida
-        tortor ut dolor tristique, ac interdum erat finibus. Nullam ut augue ac
-        urna euismod volutpat id ac felis.
+        Hi, I'm <span className="info-highlights">Prajwal Pawar.</span> A
+        passionate <span className="info-highlights">Mern Stack developer</span>{" "}
+        based in India. With a strong foundation in both frontend with{" "}
+        <span className="info-highlights">React</span> and backend with{" "}
+        <span className="info-highlights">Nodejs</span>, I enjoy building
+        fullstack applications/websites. When I'm not coding, you can find me
+        exploring new technologies, or enjoying a good cup of coffee while
+        diving into a coding challenge. Let's create something amazing together!
       </p>
 
       <motion.div
