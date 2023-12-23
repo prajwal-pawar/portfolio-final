d43c5ebe04bc363ae9add2a73e00e9f24da27934
Prajwal <prajwalpawar2303@gmail.com>
2023-12-23 11:09:12 +0530

    added animation on scroll in projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,6 +18,9 @@ const Projects = () => {
           <motion.div
             key={index}
             className="project"
+            initial={{ opacity: 0, y: 20 }}
+            // animate={{ opacity: 1, y: 0 }}
+            whileInView={{ opacity: 1, y: 0 }}
             whileHover={{ scale: 1.05 }}
             transition={{ duration: 0.3 }}
           >
